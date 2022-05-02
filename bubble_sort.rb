53ae2f112a8faeea1a6f2d3619a8a1464f5768a3
Lucas Bäckman <lucas.backman@gmail.com>
2022-05-02 11:03:23 +0300

    Make method that implements bubble sort

---
+++ bubble_sort.rb
@@ -0,0 +1,24 @@
+def bubble_sort(arr)
+  len = arr.length
+  passes = 0
+  until passes >= len - 1
+    # each pass through, need to check one fewer element
+    for i in (0..(len - 2 - passes))
+      if arr[i] > arr[i+1]
+        arr[i], arr[i+1] = arr[i+1], arr[i]
+        # p arr
+        # p "passes is #{passes}"
+        # p "index is #{i}"
+      end
+      return arr if is_sorted?(arr)
+    end
+    passes += 1
+    # p "passes after incrementing is #{passes}"
+  end
+  arr
+end
+
+def is_sorted?(arr)
+  arr.each_cons(2).all? { |a, b| a <= b }
+end
+
