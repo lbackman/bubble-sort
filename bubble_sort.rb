9e7971f416f95b1a593c4a24d8938edf4d32fdeb
Lucas Bäckman <lucas.backman@gmail.com>
2022-05-06 18:50:41 +0300

    Change code according to rubocop suggestions

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,24 +1,24 @@
+# frozen_string_literal: true
+
 def bubble_sort(arr)
   len = arr.length
   passes = 0
   until passes >= len - 1
     # each pass through, need to check one fewer element
     (len - 1 - passes).times do |i|
-      if arr[i] > arr[i+1]
-        arr[i], arr[i+1] = arr[i+1], arr[i]
-      end
+      arr[i], arr[i + 1] = arr[i + 1], arr[i] if arr[i] > arr[i + 1]
     end
-    return arr if is_sorted?(arr)
+    return arr if sorted?(arr)
     passes += 1
   end
   arr
 end
 
-def is_sorted?(arr)
+def sorted?(arr)
   arr.each_cons(2).all? { |a, b| a <= b }
 end
 
-p "Enter an array of integers separated by spaces"
-array = gets.chomp.split(" ").compact.map { |n| n = n.to_i }
+p 'Enter an array of integers separated by spaces'
+array = gets.chomp.split(' ').compact.map(&:to_i)
 p "Input array: #{array}"
 p "Sorted array: #{bubble_sort(array)}"
